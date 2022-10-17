f12a55d4c2c2a2b47b0ce3dc9428f2b7561124f6
mrgregorien <mrwilliamgregorio@gmail.com>
2022-10-17 00:20:19 -0400

    Current IP Geo Location Informatoin On Visit From Device

--- index.js
+++ index.js
@@ -24,17 +24,40 @@ function getIpInformation(ip) {
     axios.get(`https://ipinfo.io/${ip}/geo`)
         .then(res => {
             console.log(res.data);
+            document.body.appendChild(domCreation({ ip: res.data.ip, country: res.data.country, city: res.data.city, region: res.data.region, timezone: res.data.timezone, zip: res.data.postal, coordinates: res.data.loc }));
         })
         .catch(err => {
             console.log(err);
         })
 }
 
-function domCreation() {
+function domCreation({ ip, country, city, region, timezone, zip, coordinates }) {
     const container = document.createElement('div');
-    const entryPoint = document.createElement('div');
 
+    const headingTag = document.createElement('h1');
+
+    const countryTag = document.createElement('p');
+    const cityTag = document.createElement('p');
+    const regionTag = document.createElement('p');
+    const timeZoneTag = document.createElement('p');
+    const zipTag = document.createElement('p');
+    const coordinatesTag = document.createElement('p');
+    //Data Content
+    headingTag.textContent = `You Current IP: ${ip}`;
+    countryTag.textContent = `Country: ${country}`;
+    cityTag.textContent = `City: ${city}`;
+    regionTag.textContent = `Region: ${region}`;
+    timeZoneTag.textContent = `Time Zone: ${timezone}`;
+    zipTag.textContent = `Zip Code: ${zip}`;
+    coordinatesTag.textContent = `Coordinates: ${coordinates}`;
+    //Classes
+    container.classList.add('container');
+
+    //DOM 
+    container.append(headingTag, countryTag, cityTag, regionTag, timeZoneTag, zipTag, coordinatesTag);
+    return container
 }
+
 
 
 
